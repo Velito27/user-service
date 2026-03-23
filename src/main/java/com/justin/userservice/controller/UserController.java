950eb219daecaf52ddf7e60cb37edf51bc40b49f
developer <cordero.velito.27@gmail.com>
2026-03-23 09:50:39 -0500

    User-service connected to postgreSQL

--- src/main/java/com/justin/userservice/controller/UserController.java
+++ src/main/java/com/justin/userservice/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.justin.userservice.controller;
 
+import com.justin.userservice.entity.User;
+import com.justin.userservice.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
 }
